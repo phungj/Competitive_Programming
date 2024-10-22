ec80556243908a70a62aebb77d343c2bbeb97f1a
jmanrocks152 <jmanrocks152@gmail.com>
2024-10-22 03:00:56 -0500

    Completed Day 5

--- src/Advent_of_Code/2015/Day 5/main.hs
+++ src/Advent_of_Code/2015/Day 5/main.hs
@@ -1,20 +1,50 @@
-containsNaughtySubstrings :: String -> Bool
+-- Part 1
 
-containsNaughtySubstrings s = isInfixOf "ab" s or isInfixOf "cd" s or isInfixOf "pq" s or isInfixOf "xy" s
+-- import Data.List as L
 
-containsAtLeastThreeVowels :: String -> Bool
+-- containsNaughtySubstrings :: String -> Bool
 
-containsAtLeastThreeVowels s = (length $ filter (== 'a') str + length $ filter (== 'e') str + length $ filter (== 'i') str + length $ filter (== 'o') str + length $ filter (== 'u') str) > 3
+-- containsNaughtySubstrings s = L.isInfixOf "ab" s || L.isInfixOf "cd" s || L.isInfixOf "pq" s || L.isInfixOf "xy" s
 
-containsRepatedCharacter :: String -> Bool
+-- containsAtLeastThreeVowels :: String -> Bool
 
-containsRepeatedCharacter c1:c2:rest = c1 == c2 or containsRepeatedCharacter c2:rest
-containsRepeatedCharacter _ = False
+-- containsAtLeastThreeVowels s = length (filter (== 'a') s) + length (filter (== 'e') s) + length (filter (== 'i') s) + length (filter (== 'o') s) + length (filter (== 'u') s) >= 3
+
+-- containsRepeatedCharacter :: String -> Bool
+
+-- containsRepeatedCharacter (c1:c2:rest) = c1 == c2 || containsRepeatedCharacter (c2:rest)
+-- containsRepeatedCharacter _ = False
+
+-- isNiceString :: String -> Bool
+
+-- isNiceString s = not (containsNaughtySubstrings s) && containsAtLeastThreeVowels s && containsRepeatedCharacter s
+
+countNiceStrings :: [String] -> Int
+
+countNiceStrings strings = length $ filter id (map isNiceString strings)
+
+generateInputString :: String -> String
+
+generateInputString (c1:c2:c3:c4:c5:c6:c7:c8:c9:c10:c11:c12:c13:c14:c15:c16:rest) = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, '\n'] ++ generateInputString rest
+generateInputString [] = ""
+
+-- Part 2
+
+searchForRepeatedCharacterPair :: Char -> Char -> String -> Bool
+
+searchForRepeatedCharacterPair t1 t2 (c1:c2:s) = t1 == c1 && t2 == c2 || searchForRepeatedCharacterPair t1 t2 (c2:s)
+searchForRepeatedCharacterPair _ _ _ = False
+
+containsRepeatedCharacterPair :: String -> Bool
+
+containsRepeatedCharacterPair (c1:c2:s) = searchForRepeatedCharacterPair c1 c2 s || containsRepeatedCharacterPair (c2:s)
+containsRepeatedCharacterPair _ = False
+
+containsOneRepeatingLetterWithGap :: String -> Bool
+
+containsOneRepeatingLetterWithGap (c1:c2:c3:s) = c1 == c3 || containsOneRepeatingLetterWithGap (c2:c3:s)
+containsOneRepeatingLetterWithGap _ = False
 
 isNiceString :: String -> Bool
 
-isNiceString s = not containsNaughtySubstrings s and containsAtLeastThreeVowels s and containsRepatedCharacter s
-
-countNiceStrings :: [String] -> Integer
-
--- TODO: countNiceStrings strings = length $ filter (== True) 
+isNiceString s = containsRepeatedCharacterPair s && containsOneRepeatingLetterWithGap s
