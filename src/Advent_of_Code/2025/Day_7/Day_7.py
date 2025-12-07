f315c5f42f37c8e931852dfd41d1c70cc14785da
jmanrocks152 <jmanrocks152@gmail.com>
2025-12-07 04:03:30 -0600

    Completed Day 7 of AoC 2025

--- src/Advent_of_Code/2025/Day_7/Day_7.py
+++ src/Advent_of_Code/2025/Day_7/Day_7.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import heapq
+from functools import cache
 
 def count_splits(splitters, start_position):
     count = 0
@@ -8,8 +9,6 @@ def count_splits(splitters, start_position):
     used_splitters = set()
 
     while tachyons:
-        # print(tachyons)
-
         current_tachyon = heapq.heappop(tachyons)
 
         if current_tachyon not in explored:
@@ -31,10 +30,23 @@ def count_splits(splitters, start_position):
 
     return count
 
+@cache
+def count_paths_recursive(start_position):
+    global splitters
+
+    current_tachyon_col = start_position[1]
+
+    for splitter_row in splitters[current_tachyon_col]:
+        if splitter_row > start_position[0]:
+            return count_paths_recursive((splitter_row, current_tachyon_col - 1)) + count_paths_recursive((splitter_row, current_tachyon_col + 1))
+        
+    return 1
+
+splitters = defaultdict(list)
+
 with open('input.txt', 'r') as input_file:
     lines = [line.strip() for line in input_file.readlines()]
 
-    splitters = defaultdict(list)
     start_position = None
 
     for i, line in enumerate(lines):
@@ -45,4 +57,4 @@ with open('input.txt', 'r') as input_file:
                 start_position = (i, j)
 
     print(count_splits(splitters, start_position))
-    
+    print(count_paths_recursive(start_position))
